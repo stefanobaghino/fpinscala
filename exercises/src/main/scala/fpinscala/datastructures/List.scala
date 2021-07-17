51bf688fedf215cef7ecd6163df3a65f7e7cb201
Stefano Baghino <stefano@baghino.me>
2021-07-17 11:01:03 +0200

    p41: reverse

--- exercises/src/main/scala/fpinscala/datastructures/List.scala
+++ exercises/src/main/scala/fpinscala/datastructures/List.scala
@@ -113,5 +113,8 @@ object List { // `List` companion object. Contains functions for creating and wo
     go(l, z)
   }
 
+  def reverse[A](l: List[A]): List[A] =
+    foldLeft(l, Nil : List[A])((acc, a) => Cons(a, acc))
+
   def map[A,B](l: List[A])(f: A => B): List[B] = ???
 }
