6c9147f15907e046fb2994156eb8ed19950dcf0d
Stefano Baghino <stefano@baghino.me>
2021-07-17 10:52:08 +0200

    p40: length

--- exercises/src/main/scala/fpinscala/datastructures/List.scala
+++ exercises/src/main/scala/fpinscala/datastructures/List.scala
@@ -100,7 +100,7 @@ object List { // `List` companion object. Contains functions for creating and wo
   //   go(l, Nil)
   // }
 
-  def length[A](l: List[A]): Int = ???
+  def length[A](l: List[A]): Int = foldRight(l, 0)((_, l) => l + 1)
 
   def foldLeft[A,B](l: List[A], z: B)(f: (B, A) => B): B = ???
 
