af8ac4cdd4fb4e0a5e93946109846524d8141906
Stefano Baghino <stefano@baghino.me>
2021-07-17 19:49:28 +0200

    p87: Stream.{flatMap, nonNegativeLessThan}

--- exercises/src/main/scala/fpinscala/state/State.scala
+++ exercises/src/main/scala/fpinscala/state/State.scala
@@ -71,7 +71,22 @@ object RNG {
         (a :: acc, next)
     }
 
-  def flatMap[A,B](f: Rand[A])(g: A => Rand[B]): Rand[B] = ???
+  def flatMap[A,B](f: Rand[A])(g: A => Rand[B]): Rand[B] =
+    rng => {
+      val (a, next) = f(rng)
+      g(a)(next)
+    }
+
+  def nonNegativeLessThan(n: Int): Rand[Int] =
+    flatMap(nonNegativeInt) { i =>
+      val mod = i % n
+      if (i + (n-1) - mod >= 0) {
+        unit(mod)
+      } else {
+        nonNegativeLessThan(n)
+      }
+    }
+
 }
 
 case class State[S,+A](run: S => (A, S)) {
