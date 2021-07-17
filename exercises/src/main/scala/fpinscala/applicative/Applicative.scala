bd3b6dcef57657829b24bfa78eaed98e30546643
Stefano Baghino <stefano@baghino.me>
2021-07-17 21:59:29 +0200

    removing duplicate methods

--- exercises/src/main/scala/fpinscala/applicative/Applicative.scala
+++ exercises/src/main/scala/fpinscala/applicative/Applicative.scala
@@ -4,7 +4,6 @@ package applicative
 import monads.Functor
 import state._
 import State._
-import StateUtil._ // defined at bottom of this file
 import monoids._
 import language.higherKinds
 import language.implicitConversions
@@ -139,16 +138,4 @@ object Traverse {
   val optionTraverse = ???
 
   val treeTraverse = ???
-}
-
-// The `get` and `set` functions on `State` are used above,
-// but aren't in the `exercises` subproject, so we include
-// them here
-object StateUtil {
-
-  def get[S]: State[S, S] =
-    State(s => (s, s))
-
-  def set[S](s: S): State[S, Unit] =
-    State(_ => ((), s))
 }
