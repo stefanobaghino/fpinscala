373895e7b64d34a6280704dfa0bc2377a87b1d2b
Stefano Baghino <stefano@baghino.me>
2021-07-17 16:32:32 +0200

    p74: from

--- exercises/src/main/scala/fpinscala/laziness/Stream.scala
+++ exercises/src/main/scala/fpinscala/laziness/Stream.scala
@@ -74,7 +74,7 @@ object Stream {
 
   def constant(n: Int): Stream[Int] = cons(n, constant(n))
 
-  def from(n: Int): Stream[Int] = ???
+  def from(n: Int): Stream[Int] = cons(n, from(n + 1))
 
   def unfold[A, S](z: S)(f: S => Option[(A, S)]): Stream[A] = ???
 }
